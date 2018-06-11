4f8a683096b311374a36047f361d4580575fdab1
kumar.anupam <kumar.anupam@scaleswift.com>
2018-06-11 10:32:08 +0530

    Run server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 const path = require('path');
+const http = require('http')
 const express = require('express');
 const app = express();
+const socketIO = require('socket.io');
 
 const port  = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
-
+const server = http.createServer(app);
+const io = socketIO(server);
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+server.listen(port, () => {
     console.log("Listening to port 3000")
 })
